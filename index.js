438d9d417396cb468f7810ddc6b60aca774a302b
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-09-12 01:29:25 -0400

    installed package.json, mysql2, console-table-printer; added .env file

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const inqirer = require("inquirer");
 const mysql = require("mysql2");
 const {printTable} = require("console-table-printer");
+const { default: inquirer } = require("inquirer");
+require("dotenv").config();
+
+// create connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306 
+})
+db.connect(()=>{
+
+});
+
+/*
+view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role \
+*/
+function mainMenu(){
+    inquirer.prompt()
+}
