b2bb955b66fc15e7aaa7d0c6f0316e2f5a341101
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-09-12 00:33:31 -0400

    queries.sql code update

--- db/queries.sql
+++ db/queries.sql
@@ -3,4 +3,9 @@ USE employees_db;
 -- view department table --
 SELECT * FROM department;
 -- view role table --
-SELECT role.id, title, salary, name as department FROM role LEFT JOIN department on department.id = role.department_id
+SELECT role.id, title, salary, name as department FROM role LEFT JOIN department ON department.id = role.department_id;
+-- view employee table --
+SELECT employee.id, employee.first_name, employee.last_name, name as department, salary, CONCAT(supervisor.first_name, " ", supervisor.last_name) as manager FROM employee 
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON department.id = role.department_id
+LEFT JOIN employee as supervisor ON employee.manager_id = supervisor.id;
