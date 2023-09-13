060bc067d4db2fcd7f495f48646a821ccf65e151
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-09-12 22:49:23 -0400

    final index.js code

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const { printTable } = require("console-table-printer");
 require("dotenv").config();
 
 // variable for figlet
-// const figlet = require("figlet");
-// // EMPLOYEE MANAGER figlet function 
-// figlet("Employee Manager", function (err, signData) {
-//     if (err) {
-//         console.log("Something went wrong...");
-//         console.dir(err);
-//         return;
-//     }
-//     console.log(signData);
-// });
+const figlet = require("figlet");
+// EMPLOYEE MANAGER figlet function 
+figlet("Employee Manager", function (err, signData) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(signData);
+});
 
 // create connection //
 const db = mysql.createConnection({
@@ -27,7 +27,7 @@ db.connect(() => {
     mainMenu()
 });
 // Main Menu funtion //
-function mainMenu(){
+function mainMenu() {
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
@@ -36,8 +36,16 @@ function mainMenu(){
     })
         .then(answer => {
 
-            if (answer.selection === "View All Employees") {
+            if (answer.selection === "View All Departments") {
+                viewDepartments();
+            } else if (answer.selection === "View All Roles") {
+                viewRoles();
+
+            } else if (answer.selection === "View All Employees") {
                 viewEmployee();
+
+            } else if (answer.selection === "Add Role") {
+                addRole();
 
             } else if (answer.selection === "Add Employee") {
                 addEmployee();
@@ -47,8 +55,22 @@ function mainMenu(){
             }
         })
 }
+// function to view department table //
+function viewDepartments() {
+    db.query("SELECT * FROM department;", (err, allDepartments) => {
+        printTable(allDepartments);
+        mainMenu();
+    })
+}
 
-
+// function to view role table //
+function viewRoles() {
+    db.query(`SELECT role.id, title, salary, name as department FROM role LEFT JOIN department ON department.id = role.department_id;`, (err, jobRoles) => {
+        printTable(jobRoles);
+        mainMenu();
+    })
+}
+// function to view employee table //
 function viewEmployee() {
     db.query(`SELECT employee.id, employee.first_name, employee.last_name, name as department, salary, CONCAT(supervisor.first_name, " ", supervisor.last_name) as manager FROM employee  
     LEFT JOIN role ON employee.role_id = role.id
@@ -60,6 +82,35 @@ function viewEmployee() {
     })
 }
 
+// function to add a new job role //
+function addRole() {
+    db.query("SELECT id as value, name from department", (err, departmentData) => {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter a job title:",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Enter a salary:",
+                name: "salary"
+            },
+            {
+                type: "list",
+                message: "Select a Department:",
+                name: "department_id",
+                choices: departmentData
+            }
+        ]).then(answer => {
+            db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.title, answer.salary, answer.department_id], err => {
+                viewRoles();
+            })
+        })
+    })
+};
+
+// function to add a new employee //
 function addEmployee() {
     db.query("SELECT id as value, title as name from role", (err, roleData) => {
         db.query("SELECT id as value, CONCAT(first_name, ' ', last_name) as name FROM employee WHERE manager_id is null", (err, managerData) => {
@@ -94,7 +145,7 @@ function addEmployee() {
         })
     })
 };
-
+// function to update an employee //
 function updateEmployee() {
     db.query("SELECT id as value, title as name from role", (err, roleData) => {
         db.query("SELECT id as value, CONCAT(first_name, ' ', last_name) as name FROM employee", (err, employeeData) => {
@@ -110,7 +161,7 @@ function updateEmployee() {
                     message: "Select an employee:",
                     name: "employee_id",
                     choices: employeeData
-                }
+                },
             ]).then(answer => {
                 db.query("UPDATE employee SET role_id = ? WHERE id = ?;", [answer.role_id, answer.employee_id], err => {
                     viewEmployee();
